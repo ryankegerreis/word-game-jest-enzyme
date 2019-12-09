6adbf26abbd5d72aa69549d6ce81f8a5487f6bd3
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-12-09 11:54:05 -0500

    Added styling and helper functions

--- src/Components/GuessedWords.jsx
+++ src/Components/GuessedWords.jsx
@@ -17,10 +17,10 @@ function GuessedWords(props) {
 		contents = (
 			<div data-test='guessed-words'>
 				<h3>Guessed Words</h3>
-				<table>
-					<thead>
+				<table className='table table-sm'>
+					<thead className='thead-light'>
 						<tr>
-							<th>Guess</th>Matching Letters
+							<th>Guess</th><th>Matching Letters</th>
 						</tr>
 					</thead>
 					<tbody>{guessedWordsRows}</tbody>
